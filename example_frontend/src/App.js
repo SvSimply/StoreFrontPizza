62db09929ff0028c38a7c62a9ef09fcbb3cbcb6c
Jonathan <monotoastjlr@gmail.com>
2022-06-21 09:30:27 -0500

    Started React project and Add menu page HTML

---
+++ example_frontend/src/App.js
@@ -0,0 +1,15 @@
+import Header from './components/Header'
+import Menu1 from './components/Menu'
+import Footer from './components/Footer'
+
+const App = () => {
+  return (
+    <div id='MenuAppDiv'>
+      <Header />
+      <Menu1 />
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
---
+++ example_frontend/src/components/Header.js
@@ -0,0 +1,8 @@
+const Header = () => {
+  return (
+    <header>
+      <h1 id='Header'>Jimbo's Pizzeria</h1>
+    </header>
+  )
+}
+export default Header
