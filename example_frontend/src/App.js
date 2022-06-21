65193ca3a6b847cba463636da4eaa58bce4eac34
Jonathan <monotoastjlr@gmail.com>
2022-06-21 10:01:51 -0500

    Imported Homepage HTML and CSS into react project

--- example_frontend/src/App.js
+++ example_frontend/src/App.js
@@ -1,13 +1,12 @@
 import Header from './components/Header'
-import Menu1 from './components/Menu'
+import Menu1 from './components/MenuPage'
+import HomePage from './components/HomePage'
 import Footer from './components/Footer'
 
 const App = () => {
   return (
     <div id='MenuAppDiv'>
-      <Header />
-      <Menu1 />
-      <Footer />
+      <HomePage />
     </div>
   )
 }
